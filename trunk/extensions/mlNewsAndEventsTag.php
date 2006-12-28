8711e2d39149dfdbe6a76dffcf2416a036bcdc19
chucker23n <chucker23n@d8e0f0ff-9625-0410-bed6-85fe07e2cf38>
2006-12-28 11:43:05 +0000

    * Getting rid of pre-VCS version information * rewriting mlNewsAndEventsExporting to be cleaner and more efficient * extracting ICS code into exportiCalendar * exportRSS stub

--- trunk/extensions/mlNewsAndEventsTag.php
+++ trunk/extensions/mlNewsAndEventsTag.php
@@ -1,30 +1,7 @@
 <?php
 
 // MYSTlore NewsAndEvents Tag for MediaWiki
-//
-// 1.2, Soeren Kuklau, 2006-12-27
-// Make it react to any page save for now, but always export the same page
-//
-// 1.1.1, Soeren Kuklau, 2006-12-25
-// Make it not react to changes in the MYSTlore namespace
-// 
-// 1.1, Soeren Kuklau, 2006-12-25
-// iCalendar (vCalendar, .ics) exporting
-// Support for external links
-//
-// 1.0, Soeren Kuklau, 2006-12-21
-// No known issues
-//
-// 1.0b1, Soeren Kuklau, 2006-12-21
-// known bug: renderEventsCount() includes monthly headers
-//
-// 1.0a1, Soeren Kuklau, 2006-12-21
-// different naming scheme
-// renderEventsCount() mostly implemented
-// initial renderRecentEvents() and renderUpcomingEvents() stuff
-//
-// 1.0d1, Soeren Kuklau, 2006-12-17
-// Initial version
+// Soeren Kuklau, started on 2006-12-17
 
 $wgExtensionFunctions[] = "mlNewsAndEventsTag";
 $wgHooks['ArticleSaveComplete'][] = "mlNewsAndEventsExporting" ;
@@ -41,72 +18,81 @@ function mlNewsAndEventsTag() {
 }
 
 function mlNewsAndEventsExporting( &$article ) {
+	$mTitle =& $article->getTitle();
+
+	// only update if an NaE-related page was updated
+	if (strstr($mTitle->getText(), 'News and Events')) {
+		exportiCalendar();
+		exportRSS();
+	}
+
+	return true; // it'll assume an edit conflict otherwise
+}
+
+function exportiCalendar() {
 	global $wgParser;
 
-	$mTitle =& $article->getTitle();
-	$mText =& $article->getContent();
+	$naeTitle= Title::newFromURL('Template:News and Events');
+	$naeTitle->invalidateCache();
 
-//	if (strstr($mTitle->getText(), 'News and Events')) {
-		$naeTitle= Title::newFromURL('Template:News and Events');
-		$naeTitle->invalidateCache();
+	$naeArticle = new Article($naeTitle);
+	$naeText =& $naeArticle->getContent();
 
-		$naeArticle = new Article($naeTitle);
-		$naeText =& $naeArticle->getContent();
+	$wgParser->OutputType(OT_WIKI);
+	$pText =& $wgParser->parse($naeText, $naeTitle,
+		new ParserOptions(), false, true);
 
-		$wgParser->OutputType(OT_WIKI);
-		$pText =& $wgParser->parse($naeText, $naeTitle,
-			new ParserOptions(), false, true);
+	$text = $pText->getText();
 
-		$text = $pText->getText();
-
-		$text = preg_replace('/<\/h2>/', '\\1</h2>
+	$text = preg_replace('/<\/h2>/', '\\1</h2>
 ', $text); // make sure there's a linebreak after every monthly header
 
-		$array = preg_split('/[\n\r]+/', $text);
+	$array = preg_split('/[\n\r]+/', $text);
 
-		$icsOutput = "BEGIN:VCALENDAR
+	$icsOutput = "BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//MYSTlore/MYSTlore//NONSGML v1.0//EN
 ";
 
-		srand((double) microtime() * 1000000);
+	srand((double) microtime() * 1000000);
 
-		foreach( $array as $key => $value ) {
-			if (($value == '</li></ul>') || ! (preg_match('/[A-Za-z0-9]+/', $value))) {
-				unset($array[$key]);
-				continue;
-			}
+	foreach( $array as $key => $value ) {
+		if (($value == '</li></ul>') || ! (preg_match('/[A-Za-z0-9]+/', $value))) {
+			unset($array[$key]);
+			continue;
+		}
 
-			if (preg_match('/<div(.*)<h2>(.*)<\/h2>$/', $value, $matches)) { // monthly headers
-				$month = explode(' ', $matches[2]); // array with {month,year}
+		if (preg_match('/<div(.*)<h2>(.*)<\/h2>$/', $value, $matches)) { // monthly headers
+			$month = explode(' ', $matches[2]); // array with {month,year}
 
-				$months = array('January','01', 'February','02', 'March'=>'03', 'April'=>'04', 'May'=>'05', 'June'=>'06', 'July'=>'07', 'August'=>'08', 'September'=>'09', 'October'=>'10', 'November'=>'11', 'December'=>'12');
+			$months = array('January','01', 'February','02', 'March'=>'03', 'April'=>'04', 'May'=>'05', 'June'=>'06', 'July'=>'07', 'August'=>'08', 'September'=>'09', 'October'=>'10', 'November'=>'11', 'December'=>'12');
 
-				$month[0] = $months[$month[0]]; // switch from month names to month numbers
-			}
-			else  { // not a monthly header; presumed an event
-				$line = explode(': ', $value);
-				preg_match('/[0-9]{2}/', $line[0], $matches);
-				unset($line[0]);
+			$month[0] = $months[$month[0]]; // switch from month names to month numbers
+		}
+		else  { // not a monthly header; presumed an event
+			$line = explode(': ', $value);
+			preg_match('/[0-9]{2}/', $line[0], $matches);
+			unset($line[0]);
 
-				$icsOutput .= "BEGIN:VEVENT
+			$icsOutput .= "BEGIN:VEVENT
 UID:".rand(0,999999)."-".time()."@mystlore.com
 DTSTART:".$month[1].$month[0].$matches[0]."
 SUMMARY:".strip_tags(implode(': ', $line))."
 END:VEVENT
 ";
-			}
 		}
+	}
 
-		$icsOutput .= "END:VCALENDAR
+	$icsOutput .= "END:VCALENDAR
 ";
 
-		$icsFile = fopen('/data/www/chucker/myst/community/docs/wiki/Events.ics', 'w');
-		fwrite($icsFile, $icsOutput);
-		fclose($icsFile);
-//	}
+	$icsFile = fopen('/data/www/chucker/myst/community/docs/wiki/Events.ics', 'w');
+	fwrite($icsFile, $icsOutput);
+	fclose($icsFile);
+}
 
-	return true; // it'll assume an edit conflict otherwise
+function exportRSS() { // not implemented
+	
 }
 
 /* function renderAllEvents( $input, $argv, &$parser ) { // not implemented
