8fc71b3ce0a307e5f9471ddd6ff864f9fcfc8fa1
chucker23n <chucker23n@d8e0f0ff-9625-0410-bed6-85fe07e2cf38>
2007-02-13 12:02:13 +0000

    Mac OS X port of KIImageFixer

---
+++ utilities/KIImageFixer/Mac OS X/KIImageFixer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  KIImageFixer
+//
+//  Created by Sšren Kuklau on 13/02/07.
+//  Copyright chucker 2007. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
