6855636faeeb7db8579b10dbdb8a934624ece1a5
chucker23n <chucker23n@d8e0f0ff-9625-0410-bed6-85fe07e2cf38>
2007-04-18 16:03:14 +0000

    Most localization done (completely English and German; need Dutch). Some stuff left, and some strangely buggy.

---
+++ extensions/mlGZCoordinatesSpecialPage.i18n.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+	MYSTlore Great Zero Coordinates Special Page for MediaWiki
+	http://code.google.com/p/mystlore/
+
+	Created 2007-04-16 by Soeren Kuklau
+
+	Copyright 2006-07 MYSTlore contributors. Some rights reserved.
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+	The full license can be retrieved at:
+	http://www.opensource.org/licenses/mit-license.php
+*/
+
+$mlGZCoordinatesMessages = array(
+	'en' => array( 
+		'gzcoordinatesspecialpage' => 'Great Zero Coordinates',
+		'mlGZSearchForNearbyLocations' => 'Search for nearby locations:', // doesn't work yet; cf. mlGZCoordinatesSpecialPage.php#283
+		'mlGZCoordinateListPage' => 'List of known GZ coordinates',
+		'mlGZAngle' => 'Angle:',
+		'mlGZDistance' => 'Distance:',
+		'mlGZElevation' => 'Elevation:',
+		'mlGZGoButton' => 'Go',
+		'mlGZtoran' => 'toran',
+		'mlGZtorantee' => 'torantee',
+		'mlGZshahfee' => 'shahfee',
+		'mlGZshahfeetee' => 'shahfeetee',
+		'mlGZdegree' => 'degree',
+		'mlGZdegrees' => 'degrees',
+		'mlGZfoot' => 'foot',
+		'mlGZfeet' => 'feet',
+		'mlGZmeter' => 'meter',
+		'mlGZmeters' => 'meters',
+		'mlGZat' => 'at',
+		'mlGZaway' => 'away'
+	),
+	'de' => array( 
+		'gzcoordinatesspecialpage' => 'Great Zero-Koordinaten',
+		'mlGZSearchForNearbyLocations' => 'Suche nach nahe liegenden Orten:',
+		'mlGZCoordinateListPage' => 'Liste bekannter GZ-Koordinaten', // doesn't work yet; cf. mlGZCoordinatesSpecialPage.php#283
+		'mlGZAngle' => 'Winkel:',
+		'mlGZDistance' => 'Entfernung:',
+		'mlGZElevation' => 'Höhe:',
+		'mlGZGoButton' => 'Los',
+		'mlGZtoran' => 'Toran',
+		'mlGZtorantee' => 'Torantee',
+		'mlGZshahfee' => 'Shahfee',
+		'mlGZshahfeetee' => 'Shahfeetee',
+		'mlGZdegree' => 'Grad',
+		'mlGZdegrees' => 'Grad',
+		'mlGZfoot' => 'Fuß',
+		'mlGZfeet' => 'Fuß',
+		'mlGZmeter' => 'Meter',
+		'mlGZmeters' => 'Meter',
+		'mlGZat' => 'bei',
+		'mlGZaway' => 'entfernt'
+	)
+);
+
+?>
